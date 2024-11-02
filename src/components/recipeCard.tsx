d7537d66c0f45a282c4b405c910c8fac22dd0b1c
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-11-02 14:25:12 -0300

    feat: create recipeCard component

---
+++ src/components/recipeCard.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import { Recipe } from "../types";
+import { getUrlImage } from "../util/utils";
+import CheckBoxFavorites from "./checkBoxFavorites";
+import { useState } from "react";
+
+interface RecipeCardProps {
+  recipe: Recipe;
+}
+
+const RecipeCard = ({ recipe }: RecipeCardProps) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+  return (
+    <>
+      <div className="border-2 border-grey-500 rounded-md w-1/2">
+        <CheckBoxFavorites
+          id={String(recipe.ID)}
+          value={isFavorite}
+          setValue={setIsFavorite}
+        />
+        <Link to={`/recipe/${recipe.ID}`} className="flex flex-col">
+          <img
+            className=""
+            src={getUrlImage(recipe.image)}
+            alt="Recipe Image"
+          />
+          <div className="bottom-0 bg-black bg-opacity-50 text-white text-center py-1">
+            <h2 className="text-lg font-bold">{recipe.title}</h2>
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default RecipeCard;

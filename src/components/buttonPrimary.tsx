25608724d13a60933316b5ab451031dd0638a232
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-11-18 16:26:05 -0300

    feat: add ButtonPrimary component with basic props and styles

--- src/components/buttonPrimary.tsx
+++ src/components/buttonPrimary.tsx
@@ -4,17 +4,23 @@ interface ButtonPrimaryProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   color?: string;
+  width?: string;
+  paddingX?: string;
+  height?: string;
 }
 
 function ButtonPrimary({
   children,
   color = "bg-primary-primary100",
+  width = "w-full",
+  paddingX = "",
+  height = "h-14",
   ...props
 }: ButtonPrimaryProps) {
   return (
     <button
       {...props}
-      className={`${color} h-14 rounded-xl text-white w-full py-1 font-semibold flex items-center justify-center`}
+      className={`${color} ${width} ${paddingX} ${height} rounded-xl text-white py-1 font-semibold flex items-center justify-center`}
     >
       {children}
     </button>

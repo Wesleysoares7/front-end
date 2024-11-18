b0274306c51026f6bec10591e58c2397d320fe5a
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-11-18 16:29:39 -0300

    feat: enhance layout and add navigation to dashboard

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -4,17 +4,23 @@ import RecipeCard from "../../components/recipeCard";
 import useFavorites from "../../hooks/useFavorites";
 import useRedirectLogin from "../../hooks/useRedirectLogin";
 import UserContext from "../../context/userContext";
+import ButtonPrimary from "@/components/buttonPrimary";
+import { useNavigate } from "react-router-dom";
 
 const Favorites = () => {
+  const navigate = useNavigate();
   const { favorites } = useContext(FavoritesContext);
   const { user } = useContext(UserContext);
-  console.log(user);
-
   const { getFavorites } = useFavorites({
     id: "",
     value: false,
     setValue: () => {},
   });
+
+  const handleDashboard = () => {
+    navigate("/dashboard");
+  };
+
   useRedirectLogin();
 
   useEffect(() => {
@@ -22,12 +28,30 @@ const Favorites = () => {
   }, [user]);
 
   return (
-    <div>
-      <h2>Minhas Receitas Favoritas</h2>
-      <div>
-        {favorites.map((recipe) => (
-          <RecipeCard key={recipe.ID} recipe={recipe} />
-        ))}
+    <div className="flex flex-col gap-4 w-full px-2">
+      <div className="flex flex-col gap-2 pl-6">
+        <div className="flex justify-between items-center font-bold text-2xl">
+          Favorite
+          <div className="text-base">
+            <ButtonPrimary
+              children="To Back"
+              width="w-auto"
+              paddingX="px-5"
+              onClick={handleDashboard}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {favorites.length > 0 ? (
+          favorites.map((recipe) => (
+            <RecipeCard key={recipe.ID} recipe={recipe} />
+          ))
+        ) : (
+          <div className="text-center col-span-full">
+            <p>No favorite recipes found.</p>
+          </div>
+        )}
       </div>
     </div>
   );

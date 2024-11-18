134e9de055630772b528a0bb376a307f71bab65b
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-11-18 16:28:34 -0300

    feat: improve layout and styling for recipe cards

--- src/components/recipeCard.tsx
+++ src/components/recipeCard.tsx
@@ -11,25 +11,23 @@ interface RecipeCardProps {
 const RecipeCard = ({ recipe }: RecipeCardProps) => {
   const [isFavorite, setIsFavorite] = useState(false);
   return (
-    <>
-      <div className="border-2 border-grey-500 rounded-md p-1">
-        <CheckBoxFavorites
-          id={String(recipe.ID)}
-          value={isFavorite}
-          setValue={setIsFavorite}
+    <div className="border-2 border-gray-500 rounded-md p-1 w-44 h-60 flex flex-col justify-between">
+      <CheckBoxFavorites
+        id={String(recipe.ID)}
+        value={isFavorite}
+        setValue={setIsFavorite}
+      />
+      <Link to={`/recipe/${recipe.ID}`} className="flex flex-col h-full">
+        <img
+          className="object-cover w-full h-28 rounded-t-md"
+          src={getUrlImage(recipe.image)}
+          alt="Recipe Image"
         />
-        <Link to={`/recipe/${recipe.ID}`} className="flex flex-col">
-          <img
-            className=""
-            src={getUrlImage(recipe.image)}
-            alt="Recipe Image"
-          />
-          <div className="bottom-0 bg-black bg-opacity-50 text-white text-center py-1">
-            <h2 className="text-lg font-bold">{recipe.title}</h2>
-          </div>
-        </Link>
-      </div>
-    </>
+        <div className="bg-black bg-opacity-50 text-white text-center py-1">
+          <h2 className="text-sm">{recipe.title}</h2>
+        </div>
+      </Link>
+    </div>
   );
 };
 

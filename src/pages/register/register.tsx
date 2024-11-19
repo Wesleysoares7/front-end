6de7851794ed41446ce1aba8ed739679b9bba799
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2024-11-19 11:15:09 -0300

    style: improve layout and add responsive design

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -16,44 +16,46 @@ const register = () => {
   } = methods;
 
   return (
-    <FormProvider {...methods}>
-      <div className="flex flex-col h-screen w-[80%] mx-auto justify-between">
-        <form
-          onSubmit={handleSubmit(handleRegister)}
-          className="flex flex-col gap-4 justify-center h-full"
-        >
-          <div className="">
-            <h1 className="font-bold text-3xl">Create an account</h1>
-            <p className="text-netural_colour-gray1">
-              Let’s help you set up your account,
-              <br /> it won’t take long.
+    <div className="bg-custom-bg bg-contain bg-center">
+      <FormProvider {...methods}>
+        <div className="flex flex-col h-screen w-[80%] mx-auto md:w-1/3 bg-white p-4">
+          <form
+            onSubmit={handleSubmit(handleRegister)}
+            className="flex flex-col gap-4 justify-center h-full "
+          >
+            <div className="">
+              <h1 className="font-bold text-3xl">Create an account</h1>
+              <p className="text-netural_colour-gray1">
+                Let’s help you set up your account,
+                <br /> it won’t take long.
+              </p>
+            </div>
+            <div className="flex flex-col gap-10">
+              <InputField type="text" placeholder="name" formName="name" />
+              {errors.name && <p>{errors.name.message}</p>}
+              <InputField type="text" placeholder="email" formName="email" />
+              {errors.email && <p>{errors.email.message}</p>}
+              <InputField
+                type="password"
+                placeholder="password"
+                formName="password"
+              />
+              {errors.password && <p>{errors.password.message}</p>}
+              <ButtonPrimary type="submit">Registrar</ButtonPrimary>
+            </div>
+          </form>
+          <div className="flex flex-col-reverse pb-2">
+            <p className="text-center font-bold">
+              Already a member?{" "}
+              <Link to="/login" className="text-secondary-secondary100">
+                {" "}
+                Sign in
+              </Link>
             </p>
           </div>
-          <div className="flex flex-col gap-10">
-            <InputField type="text" placeholder="name" formName="name" />
-            {errors.name && <p>{errors.name.message}</p>}
-            <InputField type="text" placeholder="email" formName="email" />
-            {errors.email && <p>{errors.email.message}</p>}
-            <InputField
-              type="password"
-              placeholder="password"
-              formName="password"
-            />
-            {errors.password && <p>{errors.password.message}</p>}
-            <ButtonPrimary type="submit">Registrar</ButtonPrimary>
-          </div>
-        </form>
-        <div className="flex flex-col-reverse pb-2">
-          <p className="text-center font-bold">
-            Already a member?{" "}
-            <Link to="/login" className="text-secondary-secondary100">
-              {" "}
-              Sign in
-            </Link>
-          </p>
         </div>
-      </div>
-    </FormProvider>
+      </FormProvider>
+    </div>
   );
 };
 
